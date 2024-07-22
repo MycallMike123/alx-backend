781497f96285de9893b74846d8d79c59a8faf7b4
Michael Mwangi <michaelmichire2016@gmail.com>
2024-07-22 10:56:12 -0700

    A get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -64,7 +64,7 @@ class Server:
         page_data = self.get_page(page, page_size)
         total_pages = len(self.dataset()) // page_size
 
-        hyper_diction = {
+        hyper_dict = {
             "page_size": page_size if page_size <= len(page_data)
             else len(page_data),
             "page": page,
@@ -73,4 +73,4 @@ class Server:
             "prev_page": page - 1 if page >= 1 else None,
             "total_pages": total_pages
         }
-        return hyper_diction
+        return hyper_dict
