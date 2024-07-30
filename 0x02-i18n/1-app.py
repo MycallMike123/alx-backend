efb0812043d45df3473ac86db3b8f9b831adfa04
Michael Mwangi <michaelmichire2016@gmail.com>
2024-07-30 01:41:28 -0700

    instantiate the Babel object

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel for internationalization.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def home() -> str:
+    """Render the homepage with a welcome message."""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
